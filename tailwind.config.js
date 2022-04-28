5d8a0a260e5e8c59a57be09a78c1fb497f5232d9
Privatik3 <lol11999333@gmail.com>
2022-04-28 19:37:28 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+
+        },
+        screens: {
+            'md': {'max': '767px'}
+        }
+    },
+    plugins: [],
+}
